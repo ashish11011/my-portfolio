566d55edcf82b025407437b955b05df8a4f03c01
Ashish Bishnoi <bishnoi11011@gmail.com>
2024-03-01 13:43:48 +0530

    solved lenting problem

--- src/components/sideBar/SideBar.jsx
+++ src/components/sideBar/SideBar.jsx
@@ -2,7 +2,7 @@ import { useState } from "react"
 import {motion} from "framer-motion"
 import Links from "./links/Links"
 import "./sideBar.scss"
-import ToggleButton from "./toggleButton/toggleButton"
+import ToggleButton from "./toggleButton/ToggleButton"
 
 const SideBar = () => {
 
