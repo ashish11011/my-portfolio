a9fbd4bb52ec89022887f1aa6e7bda7892b90e49
Ashish Bishnoi <bishnoi11011@gmail.com>
2024-03-02 12:07:25 +0530

    config vite file for deployment

--- vite.config.js
+++ vite.config.js
@@ -1,8 +1,11 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react";
 
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: process.env.VITE_BASE_PATH || "/ashish_bishnoi_portfolio",
-})
+  server: {
+    port: 3000,
+    open: true,
+  },
+});
